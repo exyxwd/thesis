a1e6dd8d30efea1881f3c9211c0b3c0d180cbb65
exyxwd <exyxwd@inf.elte.hu>
2024-03-27 00:25:02 +0100

    Query to find inactive, not updated records

--- backend/src/main/java/hu/exyxwd/tisztatisza/repository/WasteRepository.java
+++ backend/src/main/java/hu/exyxwd/tisztatisza/repository/WasteRepository.java
@@ -1,11 +1,16 @@
 package hu.exyxwd.tisztatisza.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import hu.exyxwd.tisztatisza.model.Waste;
 
 @Repository
-public interface WasteRepository extends JpaRepository<Waste, Long>{
-
+public interface WasteRepository extends JpaRepository<Waste, Long> {
+    @Query("SELECT w FROM Waste w WHERE w.updateTime < :date")
+    List<Waste> findAllOlderThan(@Param("date") LocalDateTime date);
 }
