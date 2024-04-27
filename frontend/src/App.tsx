d7d4536b082c12b067aa6dbe08a66ff73fb9a66a
exyxwd <exyxwd@inf.elte.hu>
2024-04-27 23:19:50 +0200

    Main page restructure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,13 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import 'styles/app.scss';
 import 'material-symbols';
 
-import MainPage from 'pages/MainPage';
-import DashboardMain from 'components/Dashboard/DashboardMain';
 import Login from 'components/Dashboard/Login';
+import MainPage from 'components/Pages/MainPage';
 import NavBar from 'components/Navigation/NavBar';
 import Register from 'components/Dashboard/Register';
 import UpdateLogs from 'components/Dashboard/UpdateLogs';
 import UserEditor from 'components/Dashboard/UserEditor';
+import DashboardMain from 'components/Dashboard/DashboardMain';
 import ProtectedRoute from 'components/Dashboard/ProtectedRoute';
 
 // TODO About page and contact page
