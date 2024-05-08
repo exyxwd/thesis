47b54e2b39fc0e5def4c058e5a01a56f5661e381
exyxwd <exyxwd@inf.elte.hu>
2024-05-08 20:28:20 +0200

    River service rework, meter threshold instead of degree, some optimization

--- backend/src/main/java/hu/exyxwd/tisztatisza/service/RiverService.java
+++ backend/src/main/java/hu/exyxwd/tisztatisza/service/RiverService.java
@@ -5,35 +5,38 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.bedatadriven.jackson.datatype.jts.JtsModule;
 
 import org.locationtech.jts.geom.*;
-import org.springframework.stereotype.Service;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.geotools.referencing.CRS;
 import org.geotools.geometry.jts.JTS;
+import org.springframework.stereotype.Service;
+import org.springframework.core.io.ClassPathResource;
 import org.opengis.referencing.operation.MathTransform;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.*;
 import java.util.*;
+import lombok.Getter;
 import java.nio.file.Files;
 
 import hu.exyxwd.tisztatisza.model.Waste;
 import hu.exyxwd.tisztatisza.repository.WasteRepository;
 
-// TODO: review this
+// TODO: review this, optimize maybe
+
+@Getter
 @Service
 public class RiverService {
-    private ObjectMapper mapper;
-    private Map<Geometry, String> rivers;
-    private GeometryFactory geometryFactory;
+    public ObjectMapper mapper;
+    public GeometryFactory geometryFactory;
+    public Map<Geometry, String> transformedRivers;
     @Autowired
-    private WasteRepository wasteRepository;
+    public WasteRepository wasteRepository;
 
     public RiverService() {
-        this.rivers = new HashMap<>();
         this.mapper = new ObjectMapper();
         this.mapper.registerModule(new JtsModule());
         this.geometryFactory = new GeometryFactory();
+        this.transformedRivers = new HashMap<>();
     }
 
     public void loadRivers() {
@@ -42,6 +45,10 @@ public class RiverService {
             File file = new ClassPathResource("osm_rivers.geojson").getFile();
             String content = new String(Files.readAllBytes(file.toPath()));
             ObjectNode node = mapper.readValue(content, ObjectNode.class);
+
+            CoordinateReferenceSystem sourceCRS = CRS.decode("EPSG:4326");
+            CoordinateReferenceSystem targetCRS = CRS.decode("EPSG:3857");
+            MathTransform transform = CRS.findMathTransform(sourceCRS, targetCRS, true);
 
             GeometryFactory geometryFactory = new GeometryFactory();
             // Iterate through all rivers
@@ -57,17 +64,22 @@ public class RiverService {
                     }
                     // Create a LineString from the coordinates
                     LineString lineString = geometryFactory.createLineString(coordinates.toArray(new Coordinate[0]));
+                    // Transform the LineString
+                    LineString transformedLineString = (LineString) JTS.transform(lineString, transform);
                     // Extract the name of the river from the name property
                     String riverName = feature.get("properties").get("name").asText().split(",")[0].toUpperCase();
-                    this.rivers.put(lineString, riverName);
+                    // Extract the name of the river from the name property
+                    this.transformedRivers.put(transformedLineString, riverName);
                 }
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public void updateNullRivers() {
+        long startTime = System.currentTimeMillis();
+    
         List<Waste> wastes = wasteRepository.findAll();
         for (Waste waste : wastes) {
             // Calculate the nearby rivers for every waste where the rivers field is null
@@ -77,22 +89,34 @@ public class RiverService {
                 wasteRepository.save(waste);
             }
         }
+    
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("updateNullRivers took " + duration + " milliseconds");
     }
 
     public String getClosestRiver(Waste waste, double thresholdInMeters) {
         try {
             CoordinateReferenceSystem sourceCRS = CRS.decode("EPSG:4326");
-            CoordinateReferenceSystem targetCRS = CRS.decode("EPSG:32632");
-            MathTransform transform = CRS.findMathTransform(sourceCRS, targetCRS);
-    
-            Coordinate wasteCoordinates = new Coordinate(waste.getLongitude().doubleValue(), waste.getLatitude().doubleValue());
+            CoordinateReferenceSystem targetCRS = CRS.decode("EPSG:3857"); // Was 32632
+            MathTransform transform = CRS.findMathTransform(sourceCRS, targetCRS, true);
+        
+            // Ensure the coordinates are within the valid range for the projection
+            double longitude = waste.getLongitude().doubleValue();
+            double latitude = waste.getLatitude().doubleValue();
+            if (longitude < -180 || longitude > 180 || latitude < -90 || latitude > 90) {
+                System.out.println(latitude + " Illegal coordinates " + longitude);
+                throw new IllegalArgumentException("Invalid coordinates: longitude " + longitude + ", latitude " + latitude);
+            }
+        
+            Coordinate wasteCoordinates = new Coordinate(longitude, latitude);
             Point wasteLocation = (Point) JTS.transform(geometryFactory.createPoint(wasteCoordinates), transform);
-    
-            String closestRiver = null;
+        
+            String closestRiver = "";
             double closestDistance = Double.MAX_VALUE;
-            for (Map.Entry<Geometry, String> entry : this.rivers.entrySet()) {
-                Geometry riverGeometry = JTS.transform(entry.getKey(), transform);
-                double distance = wasteLocation.distance(riverGeometry);
+            for (Map.Entry<Geometry, String> entry : this.transformedRivers.entrySet()) {
+                // Geometry riverGeometry = JTS.transform(entry.getKey(), transform);
+                double distance = wasteLocation.distance(entry.getKey());
                 if (distance <= thresholdInMeters && distance < closestDistance) {
                     closestDistance = distance;
                     closestRiver = entry.getValue();
@@ -100,6 +124,7 @@ public class RiverService {
             }
             return closestRiver;
         } catch (Exception e) {
+            System.out.println("Exception in getClosestRiver: " + e.getMessage());
             e.printStackTrace();
             return null;
         }
