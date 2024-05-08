50cba8ff303d30df43755d29ddb7779992339478
exyxwd <exyxwd@inf.elte.hu>
2024-05-08 20:29:36 +0200

    Optimize waste queries

--- backend/src/main/java/hu/exyxwd/tisztatisza/repository/WasteRepository.java
+++ backend/src/main/java/hu/exyxwd/tisztatisza/repository/WasteRepository.java
@@ -3,6 +3,7 @@ package hu.exyxwd.tisztatisza.repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.lang.NonNull;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.query.Param;
@@ -11,16 +12,24 @@ import hu.exyxwd.tisztatisza.model.Waste;
 
 @Repository
 public interface WasteRepository extends JpaRepository<Waste, Long> {
+    @EntityGraph(attributePaths = { "types" })
     @Query("SELECT w FROM Waste w WHERE w.updateTime < :date")
     List<Waste> findAllOlderThan(@Param("date") LocalDateTime date);
 
+    @EntityGraph(attributePaths = { "types" })
     @Query("SELECT w FROM Waste w WHERE w.country = :country AND w.size = :size AND w.status = :status AND w.updateTime > :updateTime")
     List<Waste> findByFilters(@Param("country") Waste.WasteCountry country, @Param("size") Waste.WasteSize size,
             @Param("status") Waste.WasteStatus status, @Param("updateTime") LocalDateTime updateTime);
 
+    @EntityGraph(attributePaths = { "types" })
     @Query("SELECT w FROM Waste w WHERE w.country != :country OR w.size != :size OR w.status != :status OR w.updateTime < :updateTime")
     List<Waste> findByFiltersInverse(@Param("country") Waste.WasteCountry country, @Param("size") Waste.WasteSize size,
             @Param("status") Waste.WasteStatus status, @Param("updateTime") LocalDateTime updateTime);
 
+    @EntityGraph(attributePaths = { "types" })
     List<Waste> findByHidden(boolean hidden);
+
+    @NonNull
+    @EntityGraph(attributePaths = { "types" })
+    List<Waste> findAll();
 }
