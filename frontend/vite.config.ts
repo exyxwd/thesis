3d1ccea394352bbe2c20283cdd3627fd649271db
Pelsőczi Domokos <exyxwd@inf.elte.hu>
2024-05-22 02:31:23 +0200

    Commented changes for ssl certificates

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+// import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,13 +13,18 @@ export default defineConfig({
                 target: 'http://localhost:8080/',
                 changeOrigin: true,
                 followRedirects: true,
+                // secure: false,
             },
         },
         open: true,
         // For HMR on WSL - TODOFINAL
         watch: {
             usePolling: true,
-        }
+        },
+        // https: {
+        //     key: fs.readFileSync('./key-nopass.pem'),
+        //     cert: fs.readFileSync('./cert.pem'),
+        // }
     },
     // tsconfigPaths is so the src dir is the base url - TODOFINAL
     plugins: [react(), tsconfigPaths()],
