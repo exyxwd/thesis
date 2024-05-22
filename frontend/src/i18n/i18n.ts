090a5633fe2df243096449447739c9d66a86287c
Pelsőczi Domokos <exyxwd@inf.elte.hu>
2024-05-22 02:33:01 +0200

    Language detection from browser, error translations

--- frontend/src/i18n/i18n.ts
+++ frontend/src/i18n/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-// import LanguageDetector from 'i18next-browser-languagedetector';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import hu from './hu.json';
 import en from './en.json';
@@ -29,12 +29,13 @@ const resources = {
 };
 
 i18n
-    //   .use(LanguageDetector)
+    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         debug: true,
         resources,
         fallbackLng: 'hu',
+        supportedLngs: ['hu', 'en'],
         interpolation: {
             escapeValue: false
         },
